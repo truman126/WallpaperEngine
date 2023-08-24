934b259b2760b318236d4bdb3222458b2160d494
Truman <trumanjohnston@gmail.com>
2023-08-24 16:58:09 -0400

    Update for publishing

--- server/db/index.js
+++ server/db/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 	
 
 mongoose
-    .connect('mongodb://127.0.0.1:27017/wallpaper_engine_db', { useNewUrlParser: true })
+    .connect(process.env.MONGO_URI, { useNewUrlParser: true })
 	.catch(e => {
         console.error('Connection error', e.message)
     })
