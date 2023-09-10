3cc260b5ca2fb8186c30bd7d3145ad2487ccc7dc
Truman <trumanjohnston@gmail.com>
2023-09-10 17:36:56 -0300

    adding cors middleware

--- server/routes/user.js
+++ server/routes/user.js
@@ -2,13 +2,15 @@ const express = require('express')
 
 //controllers
 const UserController = require("../controllers/UserController");
+const corsMid = require("../middleware/corsmid")
+
 
 
 const router = express.Router()
 
 
 //log in rout
-router.post('/login', UserController.loginUser)
+router.post('/login', corsMid.allowCrossDomain, UserController.loginUser)
 
 
 // sign up route
