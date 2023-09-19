c8e9c79af6ab6a3594233fbdef01aacd093be675
Truman <trumanjohnston@gmail.com>
2023-09-19 12:31:56 -0300

    changes for dev

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 const BASE_URL = process.env.REACT_APP_BASE_URL;
-
+console.log("BASE URL", BASE_URL)
 const api = axios.create({
-    baseURL: BASE_URL
+    baseURL: `${BASE_URL}`
 })
 
 
